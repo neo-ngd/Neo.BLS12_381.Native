324ba4dc79441c69b01b3d050d6edab601e07cd6
doubiliu <liumengyu@ngd.neo.org>
2022-04-25 11:50:44 +0800

    change cp path

--- pack.sh
+++ pack.sh
@@ -5,12 +5,12 @@ cargo build --release --target x86_64-unknown-linux-gnu
 echo "build windows architecture ....."
 cargo build --release --target x86_64-pc-windows-gnu
 echo "build nuget packge ....."
-cp ./target/x86_64-apple-darwin/release/libNeo_BLS12381_Native.dylib ./runtimes/osx-x64/native/lib
-cp ./target/x86_64-apple-darwin/release/libNeo_BLS12381_Native.dylib ./runtimes/osx-arm64/native/lib
-cp ./target/x86_64-pc-windows-gnu/release/Neo_BLS12381_Native.dll ./runtimes/win10-x64/native/lib/uap10.0
-cp ./target/x86_64-pc-windows-gnu/release/Neo_BLS12381_Native.dll ./runtimes/win10-x86/native/lib/uap10.0
-cp ./target/x86_64-unknown-linux-gnu/release/libNeo_BLS12381_Native.so ./runtimes/alpine-x64/native/lib
-cp ./target/x86_64-unknown-linux-gnu/release/libNeo_BLS12381_Native.so ./runtimes/linux-x64/native/lib
-cp ./target/x86_64-unknown-linux-gnu/release/libNeo_BLS12381_Native.so ./runtimes/linux-arm64/native/lib
-cp ./target/x86_64-unknown-linux-gnu/release/libNeo_BLS12381_Native.so ./runtimes/mips64/native/lib
+cp ./target/x86_64-apple-darwin/release/libNeo_Cryptography_BLS12381_Native.dylib ./runtimes/osx-x64/native/lib
+cp ./target/x86_64-apple-darwin/release/libNeo_Cryptography_BLS12381_Native.dylib ./runtimes/osx-arm64/native/lib
+cp ./target/x86_64-pc-windows-gnu/release/Neo_Cryptography_BLS12381_Native.dll ./runtimes/win10-x64/native/lib/uap10.0
+cp ./target/x86_64-pc-windows-gnu/release/Neo_Cryptography_BLS12381_Native.dll ./runtimes/win10-x86/native/lib/uap10.0
+cp ./target/x86_64-unknown-linux-gnu/release/libNeo_Cryptography_BLS12381_Native.so ./runtimes/alpine-x64/native/lib
+cp ./target/x86_64-unknown-linux-gnu/release/libNeo_Cryptography_BLS12381_Native.so ./runtimes/linux-x64/native/lib
+cp ./target/x86_64-unknown-linux-gnu/release/libNeo_Cryptography_BLS12381_Native.so ./runtimes/linux-arm64/native/lib
+cp ./target/x86_64-unknown-linux-gnu/release/libNeo_Cryptography_BLS12381_Native.so ./runtimes/mips64/native/lib
 nuget pack
